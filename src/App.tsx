d3be6a9c06b9a4cbcb0981f7bb5cc84b98da5b1b
Rafael Sequeiros <rsequeiros@supports-MacBook-Pro.local>
2020-02-19 11:28:15 -0600

    adding ficha

--- src/App.tsx
+++ src/App.tsx
@@ -89,11 +89,11 @@ class App extends React.Component<IProps, IState> {
                 title={
                   <span>
                     <Icon type="laptop" />
-                    subnav 2
+                    Ficha
                   </span>
                 }
               >
-                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="5">Crear Ficha</Menu.Item>
                 <Menu.Item key="6">option6</Menu.Item>
                 <Menu.Item key="7">option7</Menu.Item>
                 <Menu.Item key="8">option8</Menu.Item>
